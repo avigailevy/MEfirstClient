9fa778e713ad7961aeb0fa3595b2f1d52bd03dcc
avigailevy <avigailevy929@gmail.com>
2025-06-15 20:29:04 +0300

    just started notes

---
+++ src/components/homePage/Notification.js
@@ -0,0 +1,11 @@
+import { usestate } from 'react';
+
+export function Notification({ message, type }) {
+
+    const [notes, setNotes] = usestate([]);
+
+    return (
+        <>
+        </>
+    )
+}
