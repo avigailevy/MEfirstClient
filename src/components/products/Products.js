62acf63ab43636e77bc700a614453497dded1f48
justDoItDontGiveUp <malkiesobel@gmail.com>
2025-06-22 21:04:38 +0300

    doing product

---
+++ src/components/products/Products.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { Product } from "./Product";
+import { AddProductForm } from "./AddProductForm";
+
+export function Products() {
+  const [products, setProducts] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch("http://localhost:3333/products/all");
+      if (!response.ok) throw new Error("Failed to fetch products");
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+        <SearchAndFilter/>
+      <button onClick={() => setShowForm(!showForm)}>
+        {showForm ? "בטל" : "הוסף מוצר"}
+      </button>
+
+      {showForm && <AddProductForm onSuccess={fetchProducts} />}
+
+      {products.length > 0 ? (
+        <div>
+          {products.map((product) => (
+            <div className="product-container" key={product.product_id}>
+              <Product product={product} />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>לא נמצאו מוצרים.</p>
+      )}
+    </div>
+  );
+}
