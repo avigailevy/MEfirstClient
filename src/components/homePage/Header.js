223c1b7c408d555387d74a2b681c8301ac069b6d
avigailevy <avigailevy929@gmail.com>
2025-06-24 23:57:31 +0300

    added title to navBar & added a log out button

--- src/components/homePage/Header.js
+++ src/components/homePage/Header.js
@@ -1,14 +1,17 @@
+import { Link } from 'react-router-dom';
 import { Notification } from './Notification'
 export function Header() {
 
-    // showRecentProjects = () =>{
-    //     return(
-    //     );
-    // }
+    const deleteToken = () => {
+        localStorage.clear();
+    }
 
     return (
         <div className="header-container">
-            <Notification userRole={'admin'}/>
+            <Notification userRole={'admin'} />
+            <div>
+                <Link to={`http://localhost:3000`} onClick={deleteToken}>Log out</Link>
+            </div>
         </div>
     );
 }                    
--- src/components/homePage/Notification.js
+++ src/components/homePage/Notification.js
@@ -1,66 +1,4 @@
-// import { BellPlus, BellRing } from 'lucide-react';
-// import { useState } from 'react';
-// import '../../css/Notification.css'
-
-// export function Notification({ userRole }) {
-//   const [notes, setNotes] = useState([
-//     { id: 1, text: "welcome to the ERP system" }
-//   ]);
-//   const [open, setOpen] = useState(false);
-
-//   const handleDelete = (id) => {
-//     setNotes(notes.filter(note => note.id !== id));
-//   };
-
-//   const handleAddNote = () => {
-//     const newNote = prompt("הכנס טקסט ההתראה:");
-//     if (newNote) {
-//       const id = Date.now();
-//       setNotes([...notes, { id, text: newNote }]);
-//     }
-//   };
-
-//   return (
-//     <div className="notification-container">
-//       <div className="notification-icon" onClick={() => setOpen(!open)}>
-//         <BellRing />
-//         {userRole === 'admin' && (
-//           <BellPlus 
-//             className="notification-add"
-//             onClick={(e) => {
-//               e.stopPropagation();
-//               handleAddNote();
-//             }}
-//           />
-//         )}
-//       </div>
-
-//       {/* תפריט ההתראות (אם פתוח) */}
-//       {open && (
-//         <div className="notification-popup">
-//           {notes.length === 0 ? (
-//             <div className="notification-empty">אין התראות</div>
-//           ) : (
-//             notes.map(note => (
-//               <div
-//                 key={note.id}
-//                 className="notification-note"
-//                 onClick={() => handleDelete(note.id)}
-//                 title="לחץ כדי למחוק"
-//               >
-//                 {note.text}
-//               </div>
-//             ))
-//           )}
-//         </div>
-//       )}
-//     </div>
-//   );
-
-// }
-
-
-import { BellMinus, BellPlus, BellRing } from 'lucide-react';
+import { BellDot, BellMinus, BellPlus, BellRing } from 'lucide-react';
 import { useState } from 'react';
 import '../../css/Notification.css';
 
@@ -95,7 +33,7 @@ export function Notification({ userRole }) {
   return (
     <div className="notification-container">
       <div className="notification-icon" onClick={() => setOpen(!open)}>
-        <BellRing />
+        {notes.length > 1 ? <BellDot /> : <BellRing />}
         {userRole === 'admin' && (
           <BellPlus
             className="notification-add"
