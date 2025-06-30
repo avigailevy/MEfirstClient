e0e8317079abef54b0e240f744cb9b7d7f78c9ff
avigailevy <avigailevy929@gmail.com>
2025-07-01 01:19:54 +0300

    products css

--- src/components/products/AddOrEditProductForm.js
+++ src/components/products/AddOrEditProductForm.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom"; 
+import '../../css/AddOrEditProductForm.css'
 
 export function AddOrEditProductForm({ onSuccess, product = null }) {
       const { username } = useParams();
